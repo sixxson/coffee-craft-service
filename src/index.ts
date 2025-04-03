73c1f155b5ddb3e7bf380115a5125b240b370e49
nlpn1408 <nlpn1481999@gmail.com>
2025-04-03 11:57:37 +0700

    Update CORS configuration to allow requests from localhost:3000

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "*",
+    origin: [ process.env.CORS_ORIGIN || "*" , "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow cookies to be sent with requests
